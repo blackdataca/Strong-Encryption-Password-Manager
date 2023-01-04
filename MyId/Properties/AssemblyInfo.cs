93c0a23c12d7a2494c9db783ad0afeb85d178d55
robinclarke <robinclarke@blackdata.ca>
2023-01-04 10:27:36 -0800

    Increase version to 2023

--- MyId/Properties/AssemblyInfo.cs
+++ MyId/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MyId")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BlackData")]
 [assembly: AssemblyProduct("MyId")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © All rights reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2022.1.*")]
+[assembly: AssemblyVersion("2023.1.*")]
