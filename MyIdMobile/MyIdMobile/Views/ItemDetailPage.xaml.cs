155d8fde85b8989fc3ef3dc1e9bd32ef40dc3900
robinclarke <robinclarke@blackdata.ca>
2023-08-09 11:17:28 -0700

    Added Mobile projects

---
+++ MyIdMobile/MyIdMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyIdMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MyIdMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
