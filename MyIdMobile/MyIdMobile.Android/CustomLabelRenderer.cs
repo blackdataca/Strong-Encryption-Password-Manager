eb61e331792f8347e23cc48610d6819210be923d
robinclarke <robinclarke@blackdata.ca>
2023-08-22 14:45:25 -0700

    Selectable label

---
+++ MyIdMobile/MyIdMobile.Android/CustomLabelRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MyIdMobile.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace MyIdMobile.Droid
+{
+    class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetTextIsSelectable(true);
+        }
+    }
+}
---
+++ MyIdMobile/MyIdMobile.iOS/CustomLabelRenderer.cs
@@ -0,0 +1,88 @@
+﻿using Foundation;
+using MyIdMobile.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace MyIdMobile.iOS
+{
+    public class CustomLabelRenderer : ViewRenderer<Label, UITextView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    SetNativeControl(new UITextView());
+                }
+
+                UpdateText();
+            }
+
+            base.OnElementChanged(e);
+        }
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == nameof(Label.Text))
+            {
+                UpdateText();
+            }
+        }
+        void UpdateText()
+        {
+            if (string.IsNullOrWhiteSpace(Element?.Text) && Element?.FormattedText?.Spans.Count == 0)
+            {
+                Control.Text = string.Empty;
+                return;
+            }
+
+            NSError error = null;
+            if (!string.IsNullOrWhiteSpace(Element?.Text))
+            {
+                Control.AttributedText = new NSAttributedString(NSData.FromString(Element.Text),
+                                                           new NSAttributedStringDocumentAttributes { DocumentType = NSDocumentType.PlainText },
+                                                           ref error);
+            }
+
+            if (Element?.FormattedText?.Spans.Count > 0)
+            {
+                foreach (var item in Element?.FormattedText.Spans)
+                {
+                    Control.AttributedText = new NSAttributedString(NSData.FromString(item.Text),
+                                                           new NSAttributedStringDocumentAttributes { DocumentType = NSDocumentType.PlainText },
+                                                           ref error);
+                }
+            }
+
+            switch (Element.FontAttributes)
+            {
+                case FontAttributes.None:
+                    Control.Font = UIFont.SystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+                case FontAttributes.Bold:
+                    Control.Font = UIFont.BoldSystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+                case FontAttributes.Italic:
+                    Control.Font = UIFont.ItalicSystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+                default:
+                    Control.Font = UIFont.BoldSystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+            }
+
+            Control.BackgroundColor = Element.BackgroundColor.ToUIColor();
+            // Control.TextAlignment = UITextAlignment.Justified;
+            Control.Selectable = true;
+            Control.Editable = false;
+            Control.ScrollEnabled = false;
+            Control.ShouldInteractWithUrl += delegate { return true; };
+        }
+    }
+}
