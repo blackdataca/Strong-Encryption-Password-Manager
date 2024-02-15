15cf5b7d9d1e4e03cccf6f7d087872bc59fff17b
Robin Clarke <25224498+1225g@users.noreply.github.com>
2024-02-15 15:44:20 -0800

    Added Blazor Web App and Database projects

---
+++ MyIdDatabase/Tables/logs.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[logs]
+(
+	[id] INT NOT NULL PRIMARY KEY identity, 
+    [user_id] NVARCHAR(450) NULL, 
+    [created] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
+    [description] NVARCHAR(200) NULL, 
+    [extra] NVARCHAR(200) NULL, 
+    [remote_address] VARCHAR(50) NULL, 
+    CONSTRAINT [FK_logs_users] FOREIGN KEY ([user_id]) REFERENCES [users]([id])
+)
---
+++ MyIdDatabase/Tables/secrets.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[secrets]
+(
+	[id] BINARY(16) NOT NULL PRIMARY KEY, 
+    [record_id] VARCHAR(23) NOT NULL, 
+    [payload] TEXT NOT NULL, 
+    [created] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
+    [modified] DATETIME NULL, 
+    [synced] DATETIME NULL 
+)
+
+GO
+
+
+CREATE INDEX [IX_secrets_record_id] ON [dbo].[secrets] ([record_id])
