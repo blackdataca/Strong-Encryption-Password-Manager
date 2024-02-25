a939527047057ed50481acd3ccd406a2dd849151
Robin Clarke <25224498+1225g@users.noreply.github.com>
2024-02-25 11:56:27 -0800

    Added loggedInUser

--- MyIdWeb/Models/CreateSecretModel.cs
+++ MyIdWeb/Models/CreateSecretModel.cs
@@ -6,16 +6,16 @@ public class CreateSecretModel
 {
     [Required]
     [MaxLength(50)]
-    public string Site { get; set; }
+    public string? Site { get; set; }
 
     [MaxLength(50)]
     [Display(Name = "User")]
-    public string UserName { get; set; }
+    public string? UserName { get; set; }
 
     [MaxLength(50)]
-    public string Password { get; set; }
+    public string? Password { get; set; }
 
     [MaxLength(500)]
-    public string Memo { get; set; }
+    public string? Memo { get; set; }
 
 }
